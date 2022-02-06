6b86ba5928ef81756a98dfe278ee90deba5a6ff5
Joshua Sauter <joshua.sauter@duke.edu>
2022-02-06 18:15:40 -0500

    Added basic viewing and deleting of feedback entries

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete this entry?')){
+            setFeedback(feedback.filter((item) => item.id != id))
+        }
+        
+    }
+
+    return (
+        <>
+            <Header text='Feedback App' />
+            <div className='container'>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </div>
+        </>
+    )
+}
+
+export default App;
